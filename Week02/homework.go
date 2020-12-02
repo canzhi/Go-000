4eed53fe43670a7373badeb7a33499e8f2b4a42b
canzhi <38463604+canzhi@users.noreply.github.com>
2020-12-02 23:19:53 +0800

    Update homework.go

--- Week02/homework.go
+++ Week02/homework.go
@@ -1,1 +1,8 @@
-1. 
+1. 如果DAO层，遇到sql.ErrNoRows 应该抛出错误给上层。
+if err != nil {
+  if _, ok := err.(sql.ErrNoRows); ok {
+    return errors.Wrap(err, "DAO: NO ROWS FROM DB")
+  } else {
+    return errors.Wrap(err, "DAO: not sql.ErrNoRows error")
+  }
+}
